d582505e29cdbe926e17f114107e20ae270123d6
ArsKostromin <arsyakost@gmail.com>
2025-10-09 01:45:30 +0500

    adds-gift

--- gifts/urls.py
+++ gifts/urls.py
@@ -1,7 +1,8 @@
 # urls.py
 from django.urls import path
-from .views import UserInventoryView
+from .views import UserInventoryView, UserAddsGift
 
 urlpatterns = [
     path("inventory/", UserInventoryView.as_view(), name="user-inventory"),
+    path("adds-gift/", UserAddsGift.as_view(), name="user-adds-gift"),
 ]
