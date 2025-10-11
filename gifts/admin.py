6865872986729302478ac774cbb96302dcdb9d3a
ArsKostromin <arsyakost@gmail.com>
2025-10-11 17:12:03 +0500

    админки

--- gifts/admin.py
+++ gifts/admin.py
@@ -1,52 +1,61 @@
-# # gifts/admin.py
-# from django.contrib import admin
-# from .models import Gift
+# gifts/admin.py
+from django.contrib import admin
+from .models import Gift
 
 
-# @admin.register(Gift)
-# class GiftAdmin(admin.ModelAdmin):
-#     list_display = (
-#         "id",
-#         "name",
-#         "symbol",
-#         "user",
-#         "price_ton",
-#         "rarity",
-#         "is_onchain",
-#         "created_at",
-#     )
-#     list_filter = ("rarity", "is_onchain", "created_at", "updated_at")
-#     search_fields = ("name", "symbol", "slug", "tg_nft_id", "ton_contract_address")
-#     ordering = ("-created_at",)
-#     list_editable = ("price_ton", "rarity", "is_onchain")
-#     readonly_fields = ("created_at", "updated_at")
-
-#     fieldsets = (
-#         ("Владение", {
-#             "fields": ("user",)
-#         }),
-#         ("Основная информация", {
-#             "fields": (
-#                 "name",
-#                 "description",
-#                 "image_url",
-#                 "price_ton",
-#                 "rarity",
-#             )
-#         }),
-#         ("NFT Telegram", {
-#             "fields": (
-#                 "tg_nft_id",
-#                 "symbol",
-#                 "slug",
-#                 "backdrop",
-#                 "model",
-#                 "ton_contract_address",
-#                 "decimals",
-#                 "is_onchain",
-#             )
-#         }),
-#         ("Служебное", {
-#             "fields": ("created_at", "updated_at"),
-#         }),
-#     )
+@admin.register(Gift)
+class GiftAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "user",
+        "ton_contract_address",
+        "price_ton",
+        "rarity_level",
+        "is_onchain",
+        "created_at",
+        "updated_at",
+    )
+    
+    list_filter = (
+        "is_onchain",
+        "rarity_level",
+        "created_at",
+        "updated_at",
+    )
+    
+    search_fields = (
+        "name",
+        "ton_contract_address",
+        "user__username",
+        "symbol",
+        "model_name",
+        "pattern_name",
+        "backdrop",
+    )
+    
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
+    
+    fieldsets = (
+        ("Основная информация", {
+            "fields": ("name", "user", "ton_contract_address", "symbol", "price_ton", "rarity_level")
+        }),
+        ("Визуальные компоненты", {
+            "fields": ("model_name", "pattern_name", "backdrop")
+        }),
+        ("Детали редкости", {
+            "fields": (
+                ("model_rarity_permille", "model_original_details"),
+                ("pattern_rarity_permille", "pattern_original_details"),
+                ("backdrop_rarity_permille", "backdrop_original_details"),
+            )
+        }),
+        ("Служебные", {
+            "fields": ("is_onchain", "created_at", "updated_at")
+        }),
+    )
+    
+    ordering = ("-created_at",)
+    save_on_top = True
--- transactions/admin.py
+++ transactions/admin.py
@@ -1,66 +1,66 @@
-# from django.contrib import admin
-# from .models import Transaction, TONWallet, TONTransaction
+from django.contrib import admin
+from .models import Transaction, TONWallet, TONTransaction
 
 
-# @admin.register(TONWallet)
-# class TONWalletAdmin(admin.ModelAdmin):
-#     list_display = ['user', 'address', 'subwallet_id', 'is_active', 'created_at']
-#     list_filter = ['is_active', 'created_at']
-#     search_fields = ['user__username', 'user__email', 'address']
-#     readonly_fields = ['created_at']
+@admin.register(TONWallet)
+class TONWalletAdmin(admin.ModelAdmin):
+    list_display = ['user', 'address', 'subwallet_id', 'is_active', 'created_at']
+    list_filter = ['is_active', 'created_at']
+    search_fields = ['user__username', 'user__email', 'address']
+    readonly_fields = ['created_at']
     
-#     fieldsets = (
-#         ('Основная информация', {
-#             'fields': ('user', 'address', 'subwallet_id')
-#         }),
-#         ('Статус', {
-#             'fields': ('is_active',)
-#         }),
-#         ('Даты', {
-#             'fields': ('created_at',),
-#             'classes': ('collapse',)
-#         }),
-#     )
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('user', 'address', 'subwallet_id')
+        }),
+        ('Статус', {
+            'fields': ('is_active',)
+        }),
+        ('Даты', {
+            'fields': ('created_at',),
+            'classes': ('collapse',)
+        }),
+    )
 
 
-# @admin.register(TONTransaction)
-# class TONTransactionAdmin(admin.ModelAdmin):
-#     list_display = ['user', 'tx_hash', 'amount', 'token', 'status', 'created_at']
-#     list_filter = ['status', 'token', 'created_at']
-#     search_fields = ['user__username', 'tx_hash', 'sender_address']
-#     readonly_fields = ['created_at', 'updated_at']
+@admin.register(TONTransaction)
+class TONTransactionAdmin(admin.ModelAdmin):
+    list_display = ['user', 'tx_hash', 'amount', 'token', 'status', 'created_at']
+    list_filter = ['status', 'token', 'created_at']
+    search_fields = ['user__username', 'tx_hash', 'sender_address']
+    readonly_fields = ['created_at', 'updated_at']
     
-#     fieldsets = (
-#         ('Основная информация', {
-#             'fields': ('user', 'wallet', 'tx_hash', 'amount', 'token')
-#         }),
-#         ('Статус и детали', {
-#             'fields': ('status', 'sender_address', 'block_time')
-#         }),
-#         ('Даты', {
-#             'fields': ('created_at', 'updated_at'),
-#             'classes': ('collapse',)
-#         }),
-#     )
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('user', 'wallet', 'tx_hash', 'amount', 'token')
+        }),
+        ('Статус и детали', {
+            'fields': ('status', 'sender_address', 'block_time')
+        }),
+        ('Даты', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
 
 
-# @admin.register(Transaction)
-# class TransactionAdmin(admin.ModelAdmin):
-#     list_display = ['user', 'tx_type', 'amount', 'currency', 'created_at']
-#     list_filter = ['tx_type', 'currency', 'created_at']
-#     search_fields = ['user__username', 'description']
-#     readonly_fields = ['created_at']
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['user', 'tx_type', 'amount', 'currency', 'created_at']
+    list_filter = ['tx_type', 'currency', 'created_at']
+    search_fields = ['user__username', 'description']
+    readonly_fields = ['created_at']
     
-#     fieldsets = (
-#         ('Основная информация', {
-#             'fields': ('user', 'tx_type', 'amount', 'currency', 'description')
-#         }),
-#         ('TON транзакция', {
-#             'fields': ('ton_transaction',),
-#             'classes': ('collapse',)
-#         }),
-#         ('Даты', {
-#             'fields': ('created_at',),
-#             'classes': ('collapse',)
-#         }),
-#     )
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('user', 'tx_type', 'amount', 'currency', 'description')
+        }),
+        ('TON транзакция', {
+            'fields': ('ton_transaction',),
+            'classes': ('collapse',)
+        }),
+        ('Даты', {
+            'fields': ('created_at',),
+            'classes': ('collapse',)
+        }),
+    )
