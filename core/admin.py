7e14a8803aef82dd7273251403fb65e0cacf50e6
ArsKostromin <arsyakost@gmail.com>
2025-10-11 15:24:06 +0500

    adminki

--- core/admin.py
+++ core/admin.py
@@ -1,14 +1,14 @@
-# admin.py
-from django.contrib import admin
-from .models import Config
+# # admin.py
+# from django.contrib import admin
+# from .models import Config
 
 
-@admin.register(Config)
-class ConfigAdmin(admin.ModelAdmin):
-    list_display = ("key", "value", "description")   # колонки в списке
-    search_fields = ("key", "value", "description")  # поиск
-    list_filter = ("key",)                           # фильтр по ключу
-    ordering = ("key",)                              # сортировка по ключу
-    list_editable = ("value",)                       # прямо из списка можно менять value
-    save_as = True                                   # "сохранить как новый"
-    save_on_top = True                               # кнопки "сохранить" сверху тоже
+# @admin.register(Config)
+# class ConfigAdmin(admin.ModelAdmin):
+#     list_display = ("key", "value", "description")   # колонки в списке
+#     search_fields = ("key", "value", "description")  # поиск
+#     list_filter = ("key",)                           # фильтр по ключу
+#     ordering = ("key",)                              # сортировка по ключу
+#     list_editable = ("value",)                       # прямо из списка можно менять value
+#     save_as = True                                   # "сохранить как новый"
+#     save_on_top = True                               # кнопки "сохранить" сверху тоже
