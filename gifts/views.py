f55ad765926c4380f68eafc8ef038ca865add648
ArsKostromin <arsyakost@gmail.com>
2025-10-14 22:12:49 +0500

    q

--- gifts/views.py
+++ gifts/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import status, generics
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, OpenApiResponse
 
 from .serializers import GiftSerializer, GiftWithdrawSerializer
 from .services.inventory import InventoryService
