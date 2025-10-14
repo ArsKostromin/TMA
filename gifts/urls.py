8160150b9c6f49e94aaac40085b90100ab2eccea
ArsKostromin <arsyakost@gmail.com>
2025-10-14 21:16:22 +0500

    q

--- gifts/urls.py
+++ gifts/urls.py
@@ -1,8 +1,9 @@
 # urls.py
 from django.urls import path
-from .views import UserInventoryView, UserAddsGift
+from .views import UserInventoryView, UserAddsGift, WithdrawalOfNFT
 
 urlpatterns = [
     path("inventory/", UserInventoryView.as_view(), name="user-inventory"),
     path("adds-gift/", UserAddsGift.as_view(), name="user-adds-gift"),
+    path("withdraw/", WithdrawalOfNFT.as_view(), name="withdraw-nft"),
 ]
