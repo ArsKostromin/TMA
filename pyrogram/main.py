3baecd57d8cf6be3fce63a15be706135d706ddfe
ArsKostromin <arsyakost@gmail.com>
2025-10-18 23:23:50 +0500

    й

--- pyrogram/main.py
+++ pyrogram/main.py
@@ -1,22 +1,17 @@
 import asyncio
 import logging
-# Удаляем импорты threading, uvicorn и api.server, так как они больше не нужны.
+# Удалены импорты threading, uvicorn и api.server.
 from config import LOG_FORMAT, LOG_DATE_FORMAT, LOG_LEVEL
-from core.bot import main_userbot
+from bot import main_userbot  # Изменено: core.bot -> bot
 
 # Настройка логов
 logging.basicConfig(format=LOG_FORMAT, level=getattr(logging, LOG_LEVEL), datefmt=LOG_DATE_FORMAT)
 logger = logging.getLogger(__name__)
 
-# Функция run_fastapi удалена, так как веб-сервер больше не запускается.
-
 if __name__ == "__main__":
     logger.info("🚀 Приложение запущено. Начинаю запуск Userbot.")
     
-    # 1. Удален запуск FastAPI в отдельном потоке.
-
-    # 2. Запуск userbot'а в основном потоке (asyncio).
-    # main_userbot блокирует основной поток, пока не завершит работу.
+    # Запуск userbot'а в основном асинхронном потоке.
     try:
         asyncio.run(main_userbot())
     except KeyboardInterrupt:
