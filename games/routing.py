572f230b72e00956bb927dcea8948ae62dd9a969
ArsKostromin <arsyakost@gmail.com>
2025-11-12 18:41:09 +0500

    й

--- games/routing.py
+++ games/routing.py
@@ -1,7 +1,9 @@
 # games/routing.py
 from django.urls import re_path
-from . import consumers
+from spin import consumers as spin_consumers
+from games import consumers as pvp_consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/pvp/$", consumers.PvpGameConsumer.as_asgi()),
+    re_path(r"ws/spin/$", spin_consumers.SpinGameConsumer.as_asgi()),
+    re_path(r"ws/pvp/$", pvp_consumers.PvpGameConsumer.as_asgi()),
 ]
